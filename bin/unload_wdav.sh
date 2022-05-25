4f14a2b0eab73348f9919eeda6bae4f6231bee5a
Brandon Siegel <brsiegel@microsoft.com>
2022-05-24 17:09:21 -0700

    20220524

--- bin/unload_wdav.sh
+++ bin/unload_wdav.sh
@@ -2,3 +2,4 @@
 sudo launchctl stop system/com.microsoft.fresno
 sudo launchctl unload /Library/LaunchDaemons/com.microsoft.fresno.plist
 sudo launchctl disable system/com.microsoft.fresno
+sudo killall -9 dlp_agent 2>/dev/null
